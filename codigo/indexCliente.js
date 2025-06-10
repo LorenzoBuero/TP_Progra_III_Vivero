7a4e485fc51468e9a4ea0b58969a0a60a2d0984e
LorenzoBuero <lorenzobuero@gmail.com>
2025-06-10 18:15:30 -0300

    codigo de franco

---
+++ codigo/indexCliente.js
@@ -0,0 +1,34 @@
+/* forma sin DOMContentLoaded
+
+function cargarPagina(){
+
+    const botonCargado = document.querySelector(".boton-ingresar");
+    if (botonCargado) {
+        botonCargado.addEventListener("click", function(event) {
+            event.preventDefault();
+            window.location.href = "productos.html";
+        });
+    }
+}
+
+cargarPagina(); 
+
+usar el script al final del body
+*/
+
+console.log("hola")
+
+function cargarPagina() {
+    const botonCargado = document.querySelector(".boton-ingresar");
+
+    if (botonCargado) {
+        botonCargado.addEventListener("click", function(event) {
+            event.preventDefault(); 
+            window.location.href = "pagCliente.html"; 
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", cargarPagina); 
+
+/* se puede poner en el head y en el body porque espera a cargar el documento*/
