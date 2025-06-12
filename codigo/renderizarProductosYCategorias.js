865653a18daefe19f32cff5469b2f4fa3c83d3b2
LorenzoBuero <138243046+LorenzoBuero@users.noreply.github.com>
2025-06-12 02:09:08 -0300

    Update renderizarProductosYCategorias.js

--- codigo/renderizarProductosYCategorias.js
+++ codigo/renderizarProductosYCategorias.js
@@ -2,6 +2,7 @@
 
 //TODO EL CODIGO DE ESTE ARCHIVO ES RECONTRA PROVISIONAL
 
+//PONE LAS CATEGORIAS EN PANTALLA JUNTO A SU EVENTO DE ONCLICK
 const renderizarCategorias = (categorias) => 
 {
     
@@ -20,7 +21,7 @@ const renderizarCategorias = (categorias) =>
 
 }
 
-
+//Es llamado cuando se clickea una categoria, vuelve a renderizar los productos que sean de esa categoria
 const cambiarCategoria = (idCategoria) =>
     {
         console.log("plop");
@@ -29,6 +30,8 @@ const cambiarCategoria = (idCategoria) =>
 
     }
 
+
+//renderiza los productos cuya categoria sea la misma a la pasada por parametro
 const renderizarProductos = (productos, IDCategoria) =>
 {
         HTMLAModificar = document.querySelector("#product-grid")
@@ -53,7 +56,10 @@ const renderizarProductos = (productos, IDCategoria) =>
 
 
 
-
+//obtiene los datos del json y llama a las funciones de renderizar cuando 
+//se inicializa la pagina
+//ESO ESTÁ MAL, DEBERIA RETORNAR UN ARRAY, PERO COMO EL RETORNO ES
+//UNA PROMESA ASINCRONA EL CÓDIGO QUE USA ESE VALOR RETORNADO ANDA MAL
 const obtenerJSON = async (url, cosa) => 
 {
     const response = await fetch(url);
@@ -79,18 +85,95 @@ const jsonProductos = "../JSON/productos.json";
 let arrayCategorias = obtenerJSON(jsonCategorias, -1);
 let arrayProductos = obtenerJSON(jsonProductos, 1);
 
-//esto deberia ser así
+//TODO EL CODIGO DEBERIA SER MASOMENOS ASÍ
 /*
+//FUNCIONES
+const renderizarCategorias = (categorias) => 
+{
+    
+    
+    HTMLAModificar = document.querySelector("#categorias-grid")
+    
+    
+    categorias.forEach(categoria => {
+
+        //agregar el evento con addEventListener en vez de usando el HTML
+        HTMLAModificar.innerHTML += `<div id="cat-${categoria.id}" onclick="cambiarCategoria(${categoria.id})" class="tarjeta-categoria">
+                                        <img src="${categoria.imagen}" alt="${categoria.nombre}">
+                                    </div>`;
+                           
+    });
+
+
+}
+
+
+const cambiarCategoria = (idCategoria) =>
+    {
+        console.log("plop");
+
+        renderizarProductos(JSONProductos, idCategoria)
+
+    }
+
+const renderizarProductos = (productos, IDCategoria) =>
+{
+    HTMLAModificar = document.querySelector("#product-grid")
+
+
+    HTMLAModificar.innerHTML = ""
+
+
+    productos.forEach(producto => {
+        if(producto.stock && producto.categoria === IDCategoria)
+        {
+            HTMLAModificar.innerHTML += `<div id="prod-${producto.id}" class="tarjeta-producto">
+                                            <img src="${producto.imagen}" alt="${producto.nombre}">
+                                            <p>${producto.nombre}</p>
+                                        </div>`;
+    
+        }
+                      
+    });
+
+}
+
+
+
+
+const obtenerJSON = async (url) => 
+{
+    const response = await fetch(url);
+    const arrayJson = await (response).json();
+        
+    return arrayJson;
+}
+
+obtenerJSONCategorias()
+{
+    const jsonCategorias = "../JSON/categorias.json";
+    return obtenerJSON(jsonCategorias);
+}
+obtenerJSONProductos()
+{
+    const jsonCategorias = "../JSON/productos.json";
+    return obtenerJSON(jsonProductos);
+}
+
+
+
+const jsonProductos = "../JSON/productos.json";
+
 let arrayCategorias = obtenerJSON(jsonCategorias);
 let arrayProductos = obtenerJSON(jsonProductos);
 
 
+const CATEGORIA_DEFAULT = 1;
+
 renderizarCategorias(arrayCategorias)
-renderizarProductos(arrayProductos)
+renderizarProductos(arrayProductos, CATEGORIA_DEFAULT)
 */
 
-//let categorias = obtenerJSON(jsonCategorias);
-//let productos = obtenerJSON(jsonProductos);
 
 
 
